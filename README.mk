d43bfbde869956bf17819d20dc438b9c27125616
Christian Kvalheim <christkv@gmail.com>
2012-03-18 18:56:33 +0100

    Added license and README

---
+++ README.mk
@@ -0,0 +1,15 @@
+Mongoman
+========
+Mongoman is a sample application using
+
+* Node.js
+* Mongodb
+* Websockets
+* Akhibara
+* SoundJS
+
+The game is adapted from the game made by Francesco Cottone <https://github.com/kesiev/akihabara> and all graphics and assets are copyrighted by Francesco. 
+
+Background music (Only Heroes Win at Skee Ball) by Greenleo, Contact him: <http://greenleo.bandcamp.com>
+
+All code and assets including modifications to original code by Francesco Cottone is licensed under the GPL version 2. Read the term of the license in the GPL-LICENSE.txt file.
